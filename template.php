a732aa6fd9451037df0e8ebfb445324f21f0e30c
troelslenda <troels+git@reload.dk>
2010-10-07 09:23:56 +0200

    general styles + rss icon

--- template.php
+++ template.php
@@ -24,3 +24,28 @@ function metropol_user_login_block($form){
 
   return  $name . $pass . $submit . $remember . drupal_render($form);
 }
+
+
+function metropol_feed_icon($url,$title){
+  if ($image = theme('image', drupal_get_path('theme', 'metropol').'/images/rss.png', t('RSS feed'), t('RSS feed'))) {
+    // Transform view expose query string in to drupal style arguments -- ?library=1 <-> /1
+    if ($pos = strpos($url, '?')) {
+      $base = substr($url, 0, $pos);
+      $parm = '';
+      foreach ($_GET as $key => $value) {
+        if ($key != 'q') {
+          $parm .= '/' . strtolower($value);
+        }
+      }
+
+      // Extra fix for event arrangementer?library=x, as it wants taks. id/lib. id
+      if (isset($_GET['library'])) {
+        if (arg(1) == '') {
+          $parm = '/all'.$parm;
+        }
+      }
+      $url = $base.$parm;
+    }
+    return '<a href="'. check_url($url) .'" class="feed-icon">'. $image .'<span>'. t('RSS') .'</span></a>';
+  }
+}
---
+++ views-view-fields--event-list--panel-pane-4.tpl.php
@@ -0,0 +1,30 @@
+<?php
+// $Id$
+
+// Prepare a couple of variables.
+$start = date_make_date($fields['field_datetime_value']->raw);
+$price = ($fields['field_entry_price_value']->raw < 1) ? t('Free') : intval($fields['field_entry_price_value']->raw) . ' kr.';
+?>
+<div class="calendar-leafs">
+
+  <div class="leaf <?php print $is_today ?>">
+    <div class="date"><?php print dynamo_datef($start, 'j');?></div>
+    <div class="month"><?php print dynamo_datef($start, 'M');?></div>
+    <div class="day"><?php print dynamo_datef($start, 'Y');?></div>
+    <?php if (dynamo_datef($start, 'Hi') != '0000'): ?>
+    <span class="time">
+      Kl. <?php print dynamo_datef($start, ' H:i'); ?>
+    </span>
+    <?php endif; ?>
+    
+    
+  </div>
+
+  <div class="info">
+    <span><?php print $fields['field_library_ref_nid']->content; ?></span>
+    <h4><?php print $fields['title']->content; ?></h4>
+    <?php #print $price; ?>
+  </div>
+
+</div>
+
