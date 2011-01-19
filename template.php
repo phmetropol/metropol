b93a590d4f0d3c90f46bcbd12d3052dcfce5a24f
Thomas Fini Hansen <xen@xen.dk>
2011-01-19 10:42:01 +0100

    Added div for js.

--- template.php
+++ template.php
@@ -3,7 +3,9 @@
 
 
 function metropol_ting_search_form($form){
-   
+  $form['keys']['#prefix'] = '<div id="search">';
+  $form['keys']['#suffix'] = '</div>';
+
   $form['submit']['#type'] = "image_button" ;
   $form['submit']['#src'] = drupal_get_path('theme','metropol')."/images/magnifier.png";
   $form['submit']['#attributes']['class'] = '';
